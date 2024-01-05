a7a065da7e9d2eb46f6c67f2c9f9c3b42d857a2d
Cancey Dejean <canceyd@gmail.com>
2024-01-04 18:35:42 -0600

    Added Topbar

--- components/TopBar/index.tsx
+++ components/TopBar/index.tsx
@@ -1,5 +1,109 @@
 const TopBar = ({}: {}) => {
-  return <header className="bg-white py-8 px-6">Topbar</header>
+  return (
+    <header className="bg-white py-8 px-6 flex items-center gap-5">
+      <div className="border rounded-md border-gray-300 overflow-hidden flex bg-[#F3F3F3] items-center flex-1">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="w-full border-0 py-4 px-5 text-gray-900  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm bg-transparent "
+          placeholder="Search something"
+        />
+
+        <div className="h-6 w-[1px] bg-[#858585]" />
+
+        <button className="p-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
+              stroke="#858585"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M21 21L16.65 16.65"
+              stroke="#858585"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <div className="flex items-center gap-[10px]">
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M18 8C18 6.4087 17.3679 4.88258 16.2426 3.75736C15.1174 2.63214 13.5913 2 12 2C10.4087 2 8.88258 2.63214 7.75736 3.75736C6.63214 4.88258 6 6.4087 6 8C6 15 3 17 3 17H21C21 17 18 15 18 8Z"
+              stroke="#282828"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M13.73 21C13.5542 21.3031 13.3019 21.5547 12.9982 21.7295C12.6946 21.9044 12.3504 21.9965 12 21.9965C11.6496 21.9965 11.3054 21.9044 11.0018 21.7295C10.6982 21.5547 10.4458 21.3031 10.27 21"
+              stroke="#282828"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </button>
+
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M21 15C21 15.5304 20.7893 16.0391 20.4142 16.4142C20.0391 16.7893 19.5304 17 19 17H7L3 21V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V15Z"
+              stroke="#282828"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <circle cx="20" cy="4.5" r="4" fill="#FF6A55" />
+          </svg>
+        </button>
+
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="48"
+            height="48"
+            viewBox="0 0 48 48"
+            fill="none"
+          >
+            <circle
+              cx="24"
+              cy="24"
+              r="23"
+              fill="#C4C4C4"
+              stroke="#EAEAEA"
+              stroke-width="2"
+            />
+          </svg>
+        </button>
+      </div>
+    </header>
+  )
 }
 
 export default TopBar
